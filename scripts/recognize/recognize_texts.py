8caf9316cd21c40e883fb39f879f915e244de3be
Andrei Tcibin <tsibin.andr@gmail.com>
2020-01-20 23:55:55 +0300

    Использует все ядра машины для распознавания

--- scripts/recognize/recognize_texts.py
+++ scripts/recognize/recognize_texts.py
@@ -1,3 +1,5 @@
+import multiprocessing
+from multiprocessing import Pool
 import os
 
 import cv2
@@ -9,6 +11,10 @@ from scripts.common.utils import read_file_content, get_alphabet, crop_image
 from scripts.model.neural_network import NeuralNetwork
 
 alphabet = get_alphabet()
+n_classes = len(alphabet)
+data_directory = 'data/test'
+threads = multiprocessing.cpu_count()
+unified_image_size = (10, 10)
 
 
 def from_symbol_to_category(symbol):
@@ -19,10 +25,21 @@ def from_category_to_symbol(category):
     return alphabet[category[0]]
 
 
+def recognise(input):
+    testing_index = input[0]
+    letter_dimension = input[1]
+    image_path = os.path.join(data_directory, 'image_%s.png' % testing_index)
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    letter_img = crop_image(img[letter_dimension[1]:letter_dimension[3], letter_dimension[0]:letter_dimension[2]])
+    if not letter_img.shape[0] or not letter_img.shape[1]:
+        return ''
+    resized_letter_img = cv2.resize(letter_img, dsize=unified_image_size, interpolation=cv2.INTER_CUBIC)
+    input_img = np.asarray(resized_letter_img, dtype='uint8').reshape(-1)
+    neural_network = NeuralNetwork(n_classes, unified_image_size[0] * unified_image_size[1])
+    return from_category_to_symbol(neural_network.recognize(np.array([input_img])))
+
+
 def main():
-    n_classes = len(alphabet)
-    data_directory = 'data/test'
-    unified_image_size = (10, 10)
     test_data_ids = sorted(map(lambda it: int(it.replace('image_', '').replace('.png', '')),
                                filter(lambda it: it.startswith('image'),
                                       os.listdir(data_directory))))
@@ -37,25 +54,16 @@ def main():
 
     for testing_index, current_X in enumerate(X):
         image_path = os.path.join(data_directory, 'image_%s.png' % testing_index)
-        img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        predicted_letters = []
         letter_dimensions = [dim for dim in parse_letters(1 - current_X)]
 
-        i = 0
-        for letter_dimension in letter_dimensions:
-            i += 1
-            letter_img = crop_image(img[letter_dimension[1]:letter_dimension[3], letter_dimension[0]:letter_dimension[2]])
-            if not letter_img.shape[0] or not letter_img.shape[1]:
-                continue
-            resized_letter_img = cv2.resize(letter_img, dsize=unified_image_size, interpolation=cv2.INTER_CUBIC)
-            input_img = np.asarray(resized_letter_img, dtype='uint8').reshape(-1)
+        pool = Pool(threads)
+        predicted_letters = pool.map(recognise, zip([testing_index] * len(letter_dimensions), letter_dimensions))
 
-            neural_network = NeuralNetwork(n_classes, unified_image_size[0] * unified_image_size[1])
-            predicted_letter = from_category_to_symbol(neural_network.recognize(np.array([input_img])))
-            predicted_letters.append(predicted_letter)
         actual_text = Y[testing_index].replace('\n', ' ')
         predicted_text = ''.join(predicted_letters)
         print('> ' + image_path)
+        print('>> Number of threads:   ', threads)
+        print('>> Number of letters:   ', len(letter_dimensions))
         print('>> Levenshtein distance:', distance(actual_text, predicted_text))
         print('>> Length difference:   ', len(predicted_text) - len(actual_text))
         print('>> Actual:')
